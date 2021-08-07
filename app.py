15850bbf28e2288c6fee0dea3f1d16bb909c54c3
amit-c-ai <amitchoudhary9425@gmail.com>
2021-08-07 17:02:08 +0530

    web-app

--- app.py
+++ app.py
@@ -2,14 +2,35 @@ from flask import Flask, render_template, url_for
 
 app = Flask(__name__)
 
+Posts = [
+    {
+        'author': 'amit choudhary',
+        'title': 'blog post 1',
+        'content': 'This is my first blog',
+        'date': '1 January'
+    },
+    {
+        'author': 'Akshay choudhary',
+        'title': 'blog post 2',
+        'content': 'This is my second blog',
+        'date': '2 January'
+    },
+    {
+        'author': 'Komal choudhary',
+        'title': 'blog post 3',
+        'content': 'This is my third blog',
+        'date': '3 January'
+    }
+]
+
 @app.route("/")
 @app.route("/Home")
 def hello():
-    return "<h1>Hello World!<\h1>"
+    return render_template('home.html', Posts=Posts)
 
 @app.route("/About")
 def about():
-    return "<h1>About<\h2>"
+    return render_template('about.html', title="About page")
 
 if __name__== '__main__':
-    app.run()
+    app.run(debug=True)
