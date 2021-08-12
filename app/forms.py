a94412dda7c273242fae1647234f8db4372762a5
amit-c-ai <amitchoudhary9425@gmail.com>
2021-08-12 15:12:21 +0530

    fully functioned database integration to web-app, added account page which can be accessed after login only

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, Length, Email, EqualTo
+from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
+from app.models import User
 
 class RegisterationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=27)])
@@ -8,6 +9,16 @@ class RegisterationForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user:
+            raise ValidationError('User already exist!')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user:
+            raise ValidationError('email already registered!')
 
 class LoginForm(FlaskForm):
     # username = StringField('Username', validators=[DataRequired(), Length(min=2, max=27)])
--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,11 @@
-from flask import render_template, url_for, flash, redirect
-from app import app
+from flask import render_template, url_for, flash, redirect, request
+from flask_login.utils import login_required
+from app import app, db, bcrypt
 from app.models import User, Post
 from app.forms import RegisterationForm, LoginForm
 from sqlalchemy.orm import backref
 from enum import unique
+from flask_login import login_user, current_user, logout_user, login_required
 
 Posts = [
     {
@@ -37,20 +39,43 @@ def about():
 
 @app.route('/Register', methods=['GET', 'POST'])
 def register():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = RegisterationForm()
     if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!', 'success')
-        return redirect(url_for('home'))
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
+        user = User(username=form.username.data, email=form.email.data, password=hashed_password)
+        db.session.add(user)
+        db.session.commit()
+        flash(f'Your account created successfully!', 'success')
+        return redirect(url_for('login'))
     return render_template('register.html', title="Registeration", form=form)
 
 @app.route('/Login', methods=['GET', 'POST'])
 def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('home'))
+
     form = LoginForm()
     if form.validate_on_submit():
-        if(form.email.data == 'admin@gmail.com' and form.password.data == 'Admin@1234'):
-            flash('Logged in succesfully!', 'success')
-            return redirect(url_for('home'))
+        user = User.query.filter_by(email=form.email.data).first()
+        if( user and bcrypt.check_password_hash(user.password, form.password.data)):
+            login_user(user, remember=form.remember.data)
+            next_page = request.args.get('next')
+            flash(f'Logged in successfully!', 'success')
+            return redirect(next_page) if next_page else redirect(url_for('home'))
         else:
             flash('Login Unseccesfull. Please check email and password!', 'danger')
 
     return render_template('login.html', title="Login", form=form)
+
+@app.route('/Logout')
+def logout():
+    logout_user()
+    return redirect(url_for('home'))
+
+@app.route('/Account')
+@login_required
+def account():
+    return render_template('account.html', title="Account")
