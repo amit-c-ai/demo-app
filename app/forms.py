ef827913dd4246d994d0d2bd670e94bc687d6fe4
amit-c-ai <amitchoudhary9425@gmail.com>
2021-08-13 20:51:03 +0530

    added function to update profile details including profile pictures

--- app/forms.py
+++ app/forms.py
@@ -1,4 +1,6 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 from app.models import User
@@ -26,4 +28,22 @@ class LoginForm(FlaskForm):
     password = PasswordField('Password', validators=[DataRequired()])
     remember = BooleanField('Remember me')
     submit = SubmitField('Login')
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(), Length(min=2, max=27)])
+    email = StringField('Email', validators=[DataRequired(), Email()])
+    picture = FileField('Update Profile', validators=[FileAllowed(['jpg', 'jpeg', 'png', 'gif'])])
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if (username.data != current_user.username):
+            user = User.query.filter_by(username=username.data).first()
+            if user:
+                raise ValidationError('User already exist!')
+
+    def validate_email(self, email):
+        if (email.data != current_user.email):
+            user = User.query.filter_by(email=email.data).first()
+            if user:
+                raise ValidationError('email already registered!')
     
--- app/routes.py
+++ app/routes.py
@@ -1,8 +1,10 @@
+import secrets, os
+from PIL import Image
 from flask import render_template, url_for, flash, redirect, request
 from flask_login.utils import login_required
 from app import app, db, bcrypt
 from app.models import User, Post
-from app.forms import RegisterationForm, LoginForm
+from app.forms import RegisterationForm, LoginForm, UpdateAccountForm
 from sqlalchemy.orm import backref
 from enum import unique
 from flask_login import login_user, current_user, logout_user, login_required
@@ -75,7 +77,41 @@ def logout():
     logout_user()
     return redirect(url_for('home'))
 
-@app.route('/Account')
+def save_picture(form_picture):
+    random_hex = secrets.token_hex(8)
+    _, f_ext = os.path.splitext(form_picture.filename)
+    picture_fn = random_hex + f_ext
+    picture_path = os.path.join(app.root_path + '/static/profile_pics', picture_fn)
+
+    output_size = (125, 125)
+    i = Image.open(form_picture)
+    i.thumbnail(output_size)
+    i.save(picture_path)
+
+    return picture_fn
+
+@app.route('/Account', methods=['GET', 'POST'])
 @login_required
 def account():
-    return render_template('account.html', title="Account")
+    form = UpdateAccountForm()
+    if form.validate_on_submit():
+        if form.picture.data:
+            picture_file = save_picture(form.picture.data)
+            current_user.image_file = picture_file
+
+        current_user.username = form.username.data
+        current_user.email = form.email.data
+        db.session.commit()
+        flash('Account updated successfully!', 'success')
+        return redirect(url_for('account'))
+
+    elif request.method == 'GET':
+        form.username.data = current_user.username
+        form.email.data = current_user.email
+
+    if not (os.path.exists(app.root_path + '/static/profile_pics' + current_user.image_file)):
+        current_user.image_file = 'default.jpg'
+        db.session.commit()
+
+    image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
+    return render_template('account.html', title='Account', form=form, image_file=image_file)
